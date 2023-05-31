ae3179c0aea48b5e25bd4209ca684b4a3ccb2c92
Stavros Kaparelos <skaparelos@gmail.com>
2023-05-31 11:45:25 +0300

    tiny changes

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -1,5 +1,7 @@
 import LeftSideBar from "@/components/LeftSideBar"
 import MDialog from "@/components/ui/Dialog"
+import MainSection from "@/components/MainSection"
+import RightSideBar from "@/components/RightSideBar"
 
 type Props = {
   children: React.ReactNode
@@ -12,11 +14,10 @@ const MainLayout = ({ children }: Props) => {
       <MDialog title="Twitter Clone Example" text="A frontend clone of Twitter's main feed page using NextJS + Typescript + Tailwind CSS. You can browse around."/>
       <div className="grid-cols-[auto,1fr] desktop:max-w-7xl laptop:max-w-5xl max-w-2xl mx-auto">
         <LeftSideBar />
-        <main>
-          <div className="grid grid-cols-[auto,1fr] desktop:ml-72 tablet:ml-20 min-h-screen">
-            {children}
-          </div>
-        </main>
+        <div className="grid grid-cols-[auto,1fr] desktop:ml-72 tablet:ml-20 min-h-screen">
+          <MainSection />
+          <RightSideBar />
+        </div>
       </div>
     </>
   )
