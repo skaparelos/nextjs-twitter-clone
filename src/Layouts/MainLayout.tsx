1092812797c3beffb02decdb18afe86f4802b6a7
Stavros Kaparelos <skaparelos@gmail.com>
2023-08-15 22:27:02 +0300

    Update MainLayout.tsx

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -11,7 +11,7 @@ type Props = {
 const MainLayout = ({ children }: Props) => {
   return (
     <>
-      <MDialog title="Twitter Clone Example" text="A frontend clone of Twitter's main feed page using NextJS + Typescript + Tailwind CSS. You can browse around."/>
+      <MDialog title="Twitter Clone Example" text="A frontend clone of Twitter's main feed page built by @skaparelos using NextJS + Typescript + Tailwind CSS. You can browse around."/>
       <div className="grid-cols-[auto,1fr] desktop:max-w-7xl laptop:max-w-5xl max-w-2xl mx-auto">
         <LeftSideBar />
         <div className="grid grid-cols-[auto,1fr] desktop:ml-72 tablet:ml-20 min-h-screen">
